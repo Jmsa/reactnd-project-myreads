7d7c38425a4f0a23d07305e26d04daae173d59a1
James Abercrombie <jmsabercrombie88@gmail.com>
2017-09-03 21:10:57 -0700

    refactor: Use BookShelf instead of Book to render search results
    * Replace use of Book with BookShelf for consistency
    * Set query limit for search

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,33 +1,54 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import * as BooksAPI from './../BooksAPI'
-import Book from "./Book";
+// import Book from "./Book";
+import BookShelf from './BookShelf';
 
 class SearchPage extends Component {
+
+    // TODO: Once a book is moved during search add an animation or move back to the overall view.
 
     state = {
         query: '',
         results: []
     };
 
+    /**
+     * @description Cleans up new search value and resets state or searches for new query.
+     * @constructor
+     * @param {string} value - The new query string to look up. If empty resets the results.
+     */
     updateQuery(value) {
         const query = value.trim();
         this.setState({query: query});
         if (query) {
             this.searchForBook(query);
+        } else {
+            this.setState({results: []})
         }
     };
 
+    /**
+     * @description Search for a book based on input
+     * @param {string} query - The query to use when searching
+     */
     searchForBook(query) {
-        BooksAPI.search(query)
+        BooksAPI.search(query, 20)
             .then((searchResults) => {
-                const results = searchResults || [];
-                this.setState({results: results});
+                if (searchResults.length > 0) {
+                    const results = searchResults || [];
+                    this.setState({results: results});
+                }
+                else {
+                    this.setState({results: []});
+                }
             });
     };
 
     render() {
         const {results, query} = this.state;
+        const {onShelfChange} = this.props;
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -39,18 +60,11 @@ class SearchPage extends Component {
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid">
-                        {results.map((book, index) => {
-                            return (
-                                <li key={index}>
-                                    <Book authors={book.authors}
-                                          imageLinks={book.imageLinks}
-                                          title={book.title}
-                                          shelf={book.shelf}/>
-                                </li>
-                            )
-                        })}
-                    </ol>
+                    <BookShelf books={results}
+                               title="Search Results"
+                               onShelfChange={(id, shelf) => {
+                                   onShelfChange(id, shelf);
+                               }}/>
                 </div>
             </div>
         )
